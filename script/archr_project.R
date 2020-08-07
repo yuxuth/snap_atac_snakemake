cb948cb57086847fab141d978a79cba3c0482581
yu xiang <yux.uth@gmail.com>
2020-08-07 18:19:35 -0400

    Create archr_project.R

---
+++ script/archr_project.R
@@ -0,0 +1,24 @@
+library(ArchR)
+set.seed(1)
+addArchRThreads(threads = 8) 
+addArchRGenome("Mm10")
+
+inputFiles = snakemake@input[[1]]
+outdir = snakemake@output[[1]]
+sample = snakemake@wildcards[['sample']]
+
+
+ArrowFiles <- createArrowFiles(
+    inputFiles = inputFiles,
+    sampleNames = sample,
+    filterTSS = 4, #Dont set this too high because you can always increase later
+    filterFrags = 1000, 
+    addTileMat = TRUE,
+    addGeneScoreMat = T
+)
+
+proj <- ArchRProject(
+    ArrowFiles = ArrowFiles, 
+    outputDirectory = outdir,
+    copyArrows = T #This is recommened so that you maintain an unaltered copy for later usage.
+)
